65638a7e2e00f07ec28125fdd67fd299e19a2466
Vladimir Klimenko <kvl.prime@gmail.com>
2018-08-05 00:45:34 +0300

    Google login test 0.0.5.

--- src/test/java/com/googleAccounts/AccountPage.java
+++ src/test/java/com/googleAccounts/AccountPage.java
@@ -1,9 +1,7 @@
 package com.googleAccounts;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
--- src/test/java/com/googleAccounts/LoginTest.java
+++ src/test/java/com/googleAccounts/LoginTest.java
@@ -3,9 +3,6 @@ package com.googleAccounts;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginTest extends BeforeAfter {
 
